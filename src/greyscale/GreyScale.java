b6dd2eb9e50ab337bf549a70f13c3900d94a0852
eulinle_sd2082@CEACCS.NET <eulinle_sd2082@CEACCS.NET>
2019-11-20 14:51:55 +0800

    New Commit

--- src/greyscale/GreyScale.java
+++ src/greyscale/GreyScale.java
@@ -1,4 +1,3 @@
-
 package greyscale;
 
 /**
@@ -15,42 +14,42 @@ import javax.swing.JFrame;
 
 public class GreyScale {
 
-   BufferedImage  image;
-   int width;
-   int height;
-   
-   public GreyScale() {
-   
-      try {
-         File input = new File("C:\\Users\\eulinle_sd2082\\Pictures\\Camera Roll\\images.jpg");
-         image = ImageIO.read(input);
-         width = image.getWidth();
-         height = image.getHeight();
-         
-         for(int i=0; i<height; i++) {
-         
-            for(int j=0; j<width; j++) {
-           
-               Color c = new Color(image.getRGB(j, i));
-               int red = (int)(c.getRed() * 0.299);
-               int green = (int)(c.getGreen() * 0.587);
-               int blue = (int)(c.getBlue() *0.114);
-//               Color newColor = new Color(red+green+blue,
-//              
-//               red+green+blue,red+green+blue);
-               Color newColor = new Color(0,0,0);
-               
-               image.setRGB(j,i,newColor.getRGB());
+    BufferedImage image;
+    int width;
+    int height;
+
+    public GreyScale() {
+
+        try {
+            File input = new File("C:\\Users\\eulinle_sd2082\\Pictures\\flower.jpg");
+            image = ImageIO.read(input);
+            width = image.getWidth();
+            height = image.getHeight();
+
+
+            for (int i = 0; i < height; i++) {
+
+                for (int j = 0; j < width; j++) {
+
+                    Color c = new Color(image.getRGB(j, i));
+                    
+                    if (c.getBlue() < 10 && c.getGreen() < 10 && c.getRed() < 10) {
+                        
+                    } else {
+                        Color white = new Color(255,255,255);
+                        image.setRGB(j, i, white.getRGB());
+                    }
+                }
             }
-         }
-         
-         File ouptut = new File("C:\\Users\\eulinle_sd2082\\Pictures\\Camera Roll\\images.jpg");
-         ImageIO.write(image, "jpg", ouptut);
-         
-      } catch (Exception e) {}
-   }
-   
-   static public void main(String args[]) throws Exception {
-      GreyScale obj = new GreyScale();
-   }
+
+            File ouptut = new File("C:\\Users\\eulinle_sd2082\\Pictures\\newFlower.png");
+            ImageIO.write(image, "png", ouptut);
+
+        } catch (Exception e) {
+        }
+    }
+
+    static public void main(String args[]) throws Exception {
+        GreyScale obj = new GreyScale();
+    }
 }
